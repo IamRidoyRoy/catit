9c6cb77076827a89b030dbc85de2810467d121bd
Ridoy Roy <ridoyroycse53engg@gmail.com>
2022-08-11 17:57:45 +0600

    Create post via api only logged in user

--- posts/models.py
+++ posts/models.py
@@ -2,18 +2,21 @@ from pyexpat import model
 from venv import create
 from django.db import models
 from django.contrib.auth.models import User
+
+# from posts import serializers
 # Create your models here.
 
 class Post(models.Model):
     title = models.CharField(max_length=100)
     url= models.URLField()
     poster = models.ForeignKey(User, on_delete=models.CASCADE)
-    created = models.DateTimeField(auto_created=True)
+    created = models.DateTimeField(auto_now_add=True)
 
     class Meta:
         ordering = ['-created']
 
 
-class Vote:
+class Vote(models.Model):
     vote= models.ForeignKey(User, on_delete=models.CASCADE)
-    post = models.ForeignKey(Post, on_delete=models.CASCADE) 
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    
