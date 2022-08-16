533917de442a0bc84a7b981a8d847256f3fabda0
Ridoy Roy <ridoyroycse53engg@gmail.com>
2022-08-16 16:15:38 +0600

    Add delete option to delete a post

--- posts/views.py
+++ posts/views.py
@@ -1,11 +1,11 @@
 
 from logging import exception
 from django.shortcuts import render
-from rest_framework import generics, permissions
+from rest_framework import generics, permissions, mixins,status
+from rest_framework.response import Response
 from .models import Post, Vote 
 from .serializers import PostSerializer, VoteSerializer
 from rest_framework.exceptions import ValidationError
-
 # Create your views here.
 class PostList(generics.ListCreateAPIView):
     queryset = Post.objects.all()
@@ -16,7 +16,7 @@ class PostList(generics.ListCreateAPIView):
         serializer.save(poster=self.request.user)
  
  
-class VoteCreate(generics.CreateAPIView, ):
+class VoteCreate(generics.CreateAPIView, mixins.DestroyModelMixin):
     serializer_class = VoteSerializer
     permission_classes = [permissions.IsAuthenticated]
 
@@ -32,4 +32,9 @@ class VoteCreate(generics.CreateAPIView, ):
         serializer.save(voter=self.request.user, post = Post.objects.get(pk=self.kwargs['pk'])) 
     
 
-    
+    def delete(self, request, *args, **kwargs):
+        if self.get_queryset().exists():
+            self.get_queryset().delete()
+            return Response(status=status.HTTP_204_NO_CONTENT)
+        else:
+            raise ValidationError("You never voted for this post... Silly!!")
